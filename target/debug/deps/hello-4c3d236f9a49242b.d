fc972dc58f105830d9b438ef36bd838a3ced7240
artajohan369 <artajohan369@gmail.com>
2025-10-19 20:19:40 +0400

    final

---
+++ target/debug/deps/hello-4c3d236f9a49242b.d
@@ -0,0 +1,5 @@
+/Users/artajohan/Documents/hello/target/debug/deps/hello-4c3d236f9a49242b.d: src/main.rs
+
+/Users/artajohan/Documents/hello/target/debug/deps/hello-4c3d236f9a49242b: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/artajohan/Documents/hello/target/debug/hello: /Users/artajohan/Documents/hello/src/main.rs
